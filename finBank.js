86da8ac1217c9c6d2679266f042a04e88c124ba7
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-05-13 21:49:01 +0100

    first commit

---
+++ finBank.js
@@ -0,0 +1,36 @@
+
+const toLogin = ()=>{
+    window.location.href = "./Login/login.html";
+}
+
+
+// Function to show the loader
+// function showLoader() {
+//     const loaderContainer = document.getElementById('loaderContainer');
+//     if (loaderContainer) {
+//       loaderContainer.style.display = 'block'; // Display the loader
+//     }
+//   }
+  
+//   // Function to hide the loader and show the content
+//   function hideLoaderAndShowContent() {
+//     const loaderContainer = document.getElementById('loaderContainer');
+//     if (loaderContainer) {
+//       loaderContainer.style.display = 'none'; // Hide the loader
+//     }
+  
+//     // Show the content of the page
+//     document.body.style.visibility = 'visible'; // Example: Show the body content
+//   }
+  
+//   // Event listener for page load
+//   window.onload = function() {
+//     // Show the loader when the page starts loading
+//     showLoader();
+  
+//     // Set a delay (in milliseconds) before hiding the loader and showing the content
+//     const loaderDisplayDuration = 3000; // Adjust the display duration in milliseconds (e.g., 3000 = 3 seconds)
+  
+//     setTimeout(hideLoaderAndShowContent, loaderDisplayDuration);
+//   };
+  
