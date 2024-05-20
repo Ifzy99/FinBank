a9cde4034b1cecd7521327c9f9bada50ecdcfd3d
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2024-05-20 15:07:05 +0100

    worked on the dashboard page and also added some functionality to the javascript file

--- finBank.js
+++ finBank.js
@@ -1,155 +1,147 @@
+const toLogin = () => {
+  window.location.href = "login.html";
+};
 
-const toLogin = ()=>{
-    window.location.href = "./Login/login.html";
+
+// Function to get user data from local storage or initialize an empty array
+function getUserData() {
+  const userData = JSON.parse(localStorage.getItem("userDataList")) || [];
+  return userData;
+}
+
+// Function to store user data in local storage
+function storeUserData(userData) {
+  localStorage.setItem("userDataList", JSON.stringify(userData));
+}
+
+// Function to handle signup
+function signUp() {
+  const userMail = document.getElementById("userMail").value;
+  const phoneNum = document.getElementById("phoneNum").value;
+  const accountNum = document.getElementById("accountNum").value;
+  const bvnNum = document.getElementById("bvnNum").value;
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const middleName = document.getElementById("middleName").value;
+  const gender = document.getElementById("gender").value;
+  const dob = document.getElementById("dob").value;
+  const address = document.getElementById("address").value;
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value;
+  const country = document.getElementById("country").value;
+  const work = document.getElementById("work").value;
+  const password = document.getElementById("password").value;
+  const pin = document.getElementById("pin").value;
+
+  // Get existing user data or initialize an empty array
+  const userData = getUserData();
+
+  // Create a new user object
+  const newUser = {
+    userMail,
+    phoneNum,
+    accountNum,
+    bvnNum,
+    firstName,
+    lastName,
+    middleName,
+    gender,
+    dob,
+    address,
+    city,
+    state,
+    country,
+    work,
+    password,
+    pin,
+    balance: 5000,
+    airtimeHistory: [],
+    transferHistory: [],
+  };
+
+  // Add the new user to the userData array
+  userData.push(newUser);
+  console.log(newUser);
+
+  // Store the updated userData in local storage
+  storeUserData(userData);
+
+  // Redirect to the login file
+  window.location.href = "login.html";
 }
 
 
-// Function to show the loader
 
-// function showLoader() {
-//     const loaderContainer = document.getElementById('loaderContainer');
-//     if (loaderContainer) {
-//       loaderContainer.style.display = 'block'; // Display the loader
-//     }
-//   }
+// LOGIN FUNCTION 
+// Function to get user data from local storage
+function getUserData() {
+    return JSON.parse(localStorage.getItem('userDataList')) || [];
+  }
   
-//   // Function to hide the loader and show the content
-//   function hideLoaderAndShowContent() {
-//     const loaderContainer = document.getElementById('loaderContainer');
-//     if (loaderContainer) {
-//       loaderContainer.style.display = 'none'; // Hide the loader
-//     }
+  // Function to handle login
+  function login() {
+    const loginEmail = document.getElementById('loginEmail').value;
+    const loginPassword = document.getElementById('loginPassword').value;
   
-//     // Show the content of the page
-//     document.body.style.visibility = 'visible'; // Example: Show the body content
-//   }
+    const userData = getUserData();
   
-//   // Event listener for page load
-//   window.onload = function() {
-//     // Show the loader when the page starts loading
-//     showLoader();
+    const user = userData.find(
+      (user) => user.userMail === loginEmail && user.password === loginPassword
+    );
   
-//     // Set a delay (in milliseconds) before hiding the loader and showing the content
-//     const loaderDisplayDuration = 3000; // Adjust the display duration in milliseconds (e.g., 3000 = 3 seconds)
+    const alertContainer = document.getElementById('alert-container');
+    alertContainer.innerHTML = ''; // Clear any previous alert
   
-//     setTimeout(hideLoaderAndShowContent, loaderDisplayDuration);
-//   };
+    if (user) {
+      // Login successful
+      const successAlert = document.createElement('div');
+      successAlert.classList.add('alert', 'alert-success');
+      successAlert.textContent = 'Login successful!';
+      alertContainer.appendChild(successAlert);
   
-
-// // Function to retrieve existing client data from local storage or initialize an empty array
-// let allCustomers = JSON.parse(localStorage.getItem("localAllCustomers")) || [];
-
-// const signUp = () => {
-//     // Retrieve form input values
-//     const userMail = document.getElementById('userMail').value;
-//     const phoneNum = document.getElementById('phoneNum').value;
-//     const accountNum = document.getElementById('accountNum').value;
-//     const bvnNum = document.getElementById('bvnNum').value;
-//     const firstName = document.getElementById('firstName').value;
-//     const lastName = document.getElementById('lastName').value;
-//     const middleName = document.getElementById('middleName').value;
-//     const gender = document.getElementById('gender').value;
-//     const dob = document.getElementById('dob').value;
-//     const address = document.getElementById('address').value;
-//     const city = document.getElementById('city').value;
-//     const state = document.getElementById('state').value;
-//     const country = document.getElementById('country').value;
-//     const work = document.getElementById('work').value;
-//     const password = document.getElementById('password').value;
-//     const pin = document.getElementById('pin').value;
+      // Redirect to the dashboard after a short delay
+      setTimeout(() => {
+        window.location.href = 'dashboard.html';
+        window.onload = function() {
+            displayBalanceAndAccountNumber();
+          };
+      }, 2000); 
+    
+    } else {
+      // Login failed
+      const failureAlert = document.createElement('div');
+      failureAlert.classList.add('alert', 'alert-danger');
+      failureAlert.textContent = 'Invalid email or password.';
+      alertContainer.appendChild(failureAlert);
+    }
+  }
 
 
-//     // Create client data object
-//     const customersData = {
-//         Email: userMail,
-//         PhoneNumber: phoneNum,
-//         AccountNumber: accountNum,
-//         BankVerificationNumber: bvnNum,
-//         FirstName: firstName,
-//         LastName: lastName,
-//         MiddleName: middleName,
-//         Password: password,
-//         Pin: pin,
-//         Address: address,
-//         City: city,
-//         State: state,
-//         Country: country,
-//         DateOfBirth: dob,
-//         Gender: gender,
-//         Occupation: work,
-//         allTodo: [],
-//         balance: 5000,
-//         airtimeHistory: [],
-//         transferHistory: []
-//     };
+  function displayBalanceAndAccountNumber() {
+    // Retrieve user data from local storage
+    const userData = getUserData();
+  
+    // Check if user data exists
+    if (userData.length > 0) {
+      // Assuming there's only one user, get the first user object
+      const user = userData[0];
 
-//     // Add new client data to the existing clients array
-//     // allCustomers.push(customersData);
-//     console.log(customersData);
-
-//     // Store updated clients array in local storage
-//     // localStorage.setItem("localAllCustomers", JSON.stringify(allCustomers));
-
-//     // Redirect to login page after successful signup
-//     // window.location.href = "./Login/login.html";
-// };
-
-document.addEventListener('DOMContentLoaded', function() {
-    const signupForm = document.getElementById('signupForm');
-
-    signupForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent default form submission
-
-        // Get form values
-        const userMail = document.getElementById('userMail').value;
-        const phoneNum = document.getElementById('phoneNum').value;
-        const accountNum = document.getElementById('accountNum').value;
-        const bvnNum = document.getElementById('bvnNum').value;
-        const firstName = document.getElementById('firstName').value;
-        const lastName = document.getElementById('lastName').value;
-        const middleName = document.getElementById('middleName').value;
-        const gender = document.getElementById('gender').value;
-        const dob = document.getElementById('dob').value;
-        const address = document.getElementById('address').value;
-        const city = document.getElementById('city').value;
-        const state = document.getElementById('state').value;
-        const country = document.getElementById('country').value;
-        const work = document.getElementById('work').value;
-        const password = document.getElementById('password').value;
-        const pin = document.getElementById('pin').value;
-
-        // Create user object with form data
-        const user = {
-            Email: userMail,
-            PhoneNumber: phoneNum,
-            AccountNumber: accountNum,
-            BankVerificationNumber: bvnNum,
-            FirstName: firstName,
-            LastName: lastName,
-            MiddleName: middleName,
-            Password: password,
-            Pin: pin,
-            Address: address,
-            City: city,
-            State: state,
-            Country: country,
-            DateOfBirth: dob,
-            Gender: gender,
-            Occupation: work,
-            allTodo: [],
-            balance: 5000,
-            airtimeHistory: [],
-            transferHistory: []
-        };
-
-        // Store user data in local storage
-        localStorage.setItem('user', JSON.stringify(user));
-
-        // Display success message (for demonstration)
-        alert('Signup successful! User data saved in local storage.');
-
-        // Clear form fields
-        signupForm.reset();
-    });
-});
-
+      //update username
+      const userName = document.getElementById('userName');
+      if(userName){
+        userName.innerHTML = `<h3>${user.firstName}</h3>`
+      }
+  
+      // Update available balance
+      const userAvailableBalance = document.getElementById('userAvailableBalance');
+      if (userAvailableBalance) {
+         userAvailableBalance.innerHTML =`<h3>${user.balance}</h3>`;
+      }
+  
+      // Update account number
+      const userAcctNumber = document.getElementById('userAcctNumber');
+      if (userAcctNumber) {
+        userAcctNumber = `<h3>${user.accountNum}</h3>`;
+      }
+    }
+  }
